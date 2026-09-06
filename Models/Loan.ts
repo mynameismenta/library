3a7c58c64624f047ebd28b2044799365e335bd99
Pedro <p20200177@gmail.com>
2026-09-06 11:18:00 -0300

    feat: added Loan model.

---
+++ Models/Loan.ts
@@ -0,0 +1,30 @@
+import mongoose, { type ObjectId } from "mongoose";
+
+export interface ILoan {
+    userId: ObjectId,
+    bookId: ObjectId,
+    borrowedAt: Date,
+    dueDate: Date,
+    returnedAt: Date | null,
+    status: string
+}
+
+export const LoanSchema = new mongoose.Schema<ILoan>({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user"
+    },
+    bookId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "book"
+    },
+    borrowedAt: Date,
+    dueDate: Date,
+    returnedAt: Date || null,
+    status: {
+        type: String,
+        enum: ["active", "returned"]
+    }
+});
+
+export const LoanModel = mongoose.model("loan", LoanSchema);
